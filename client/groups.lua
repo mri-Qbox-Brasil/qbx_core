c02353071b4d121ccbbee4a66551b6d60cd63db6
Manason <clanerp@gmail.com>
2024-02-13 06:46:53 -0800

    started on context menu for managing groups

--- client/groups.lua
+++ client/groups.lua
@@ -1,4 +1,6 @@
+---@type table<string, Job>
 local jobs = {}
+---@type table<string, Job>
 local gangs = {}
 
 ---@return table<string, Job>
@@ -22,3 +24,60 @@ end)
 RegisterNetEvent('qbx_core:client:onGangUpdate', function(gangName, gang)
     gangs[gangName] = gang
 end)
+
+local function openSpecificJobAdminMenu(name)
+    local job = jobs[name]
+
+    local options = {
+        {
+            title = 'Edit Job',
+        }
+    }
+
+    lib.registerContext({
+        id = 'group_admin_menu_job',
+        title = 'Modify ' .. name,
+        onBack = OpenJobAdminMenu,
+        options = options,
+    })
+    lib.showContext('group_admin_menu_job')
+end
+
+function OpenJobAdminMenu()
+    local options = {}
+    for name in pairs(jobs) do
+        options[#options+1] = {
+            title = name,
+            onSelect = function()
+                openSpecificJobAdminMenu(name)
+            end
+        }
+    end
+
+    lib.registerContext({
+        id = 'group_admin_menu_jobs',
+        title = 'Modify Jobs',
+        menu = 'group_admin_menu_main',
+        options = options,
+    })
+    lib.showContext('group_admin_menu_jobs')
+end
+
+lib.registerContext({
+    id = 'group_admin_menu_main',
+    title = 'Modify Groups',
+    options = {
+        {
+            title = 'Jobs',
+            description = 'Modify jobs',
+            arrow = true,
+            icon = 'bars',
+        },
+        {
+            title = 'Gangs',
+            description = 'Modify gangs',
+            arrow = true,
+            icon = 'bars',
+        }
+    }
+})
