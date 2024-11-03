950d80ab49092c68e7151bc6fd5c0454c221aaf1
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-11-03 15:24:41 -0300

    update: configs

--- config/server.lua
+++ config/server.lua
@@ -60,7 +60,7 @@ return {
     ---@alias ColumnName string
     ---@type [TableName, ColumnName][]
     characterDataTables = {
-        {'properties', 'owner'},
+        {'properties', 'owner_citizenid'},
         {'bank_accounts_new', 'id'},
         {'playerskins', 'citizenid'},
         {'player_mails', 'citizenid'},
